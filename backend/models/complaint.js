d0c9229d6513e0dae92251ea3cd51d038ea0385b
takbir-hasan <takbirhasan274@gmail.com>
2025-09-23 17:24:02 +0600

    feat: initialize frontend with Next.js, add essential configuration files, and implement global styles; include SVG assets and README for project setup

--- backend/models/complaint.js
+++ backend/models/complaint.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const complaintSchema = new mongoose.Schema({
+  // user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   category: { type: String, default: "complaint" }, // always complaint
   type: { type: String }, // IT Issue | Facility Issue | Admin Issue
   answer: { type: String }, // answer to the complaint
